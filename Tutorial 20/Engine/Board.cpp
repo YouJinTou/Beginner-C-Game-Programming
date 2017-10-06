37252302be264f6f015c77d2d9adf6cd2eb6eb41
Dimitar Dimitrov <dandreevd@gmail.com>
2017-10-06 10:05:50 +0300

    Added DrawBorder().

--- Tutorial 20/Engine/Board.cpp
+++ Tutorial 20/Engine/Board.cpp
@@ -1,15 +1,19 @@
 #include "Board.h"
 
 Board::Board(Graphics & gfx) :
-	gfx(gfx)
-{
+	gfx(gfx) {
 }
 
-void Board::Update(const Keyboard & kb)
-{
+void Board::Update(const Keyboard & kb) {
 }
 
-void Board::Draw()
-{
+void Board::Draw() const {
+	DrawBorder();
+}
+
+void Board::DrawBorder() const {
+	gfx.DrawRect(LeftWall, WallCollor);
+	gfx.DrawRect(TopWall, WallCollor);
+	gfx.DrawRect(RightWall, WallCollor);
 }
 
--- Tutorial 20/Engine/Board.h
+++ Tutorial 20/Engine/Board.h
@@ -2,12 +2,24 @@
 
 #include "Graphics.h"
 #include "Keyboard.h"
+#include "Vec2.h"
+#include "Rect.h"
 
 class Board {
 public:
 	Board(Graphics& gfx);
 	void Update(const Keyboard & kb);
-	void Draw();
+	void Draw() const;
 private:
 	Graphics& gfx;
+	static constexpr int BorderPadding = 10;
+	const int Width = gfx.ScreenWidth - 2 * BorderPadding;
+	const int Height = gfx.ScreenHeight - BorderPadding;
+	const Vec2 TopLeft{ BorderPadding, BorderPadding };
+	const Rect LeftWall{ 0, 0, BorderPadding, gfx.ScreenHeight };
+	const Rect TopWall{ BorderPadding, 0, gfx.ScreenWidth - BorderPadding, BorderPadding };
+	const Rect RightWall{ gfx.ScreenWidth - BorderPadding, 0, gfx.ScreenWidth, gfx.ScreenHeight };
+	const Color WallCollor{ Colors::LightGray };
+
+	void DrawBorder() const;
 };
